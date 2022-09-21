581503c99cc9d728d0fb46b105fdb93b2d1b241d
Patrick T. Waugh <patrick.waugh@icloud.com>
2022-09-20 19:57:01 -0500

    Initial Commit

---
+++ Car List Challenge/Car_List_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_List_ChallengeApp.swift
+//  Car List Challenge
+//
+//  Created by Patrick Waugh on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Car_List_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Car List Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Car List Challenge
+//
+//  Created by Patrick Waugh on 9/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
